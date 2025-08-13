5f40abf5fe28ca8cbcf04c9abaef02d3deb50362
sebastiansokolowski <ssokolowskisebastian@gmail.com>
2025-08-13 23:13:52 +0200

    solution

--- db/models.py
+++ db/models.py
@@ -1,3 +1,5 @@
+from tkinter.constants import CASCADE
+
 from django.db import models
 
 
@@ -14,3 +16,37 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE,
+                                    related_name="cinema_halls")
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE,
+                              related_name="movies")
+
+    def __str__(self) -> str:
+        return f"{self.movie.title} {self.show_time}"
